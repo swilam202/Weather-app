889a749621e85dee2da84c41fe611177d03c7bad
swilam202 <mahmoudswilam24@gmail.com>
2023-03-04 23:34:33 +0200

    i designed the home page for the app and fixed the problem in the api link bucause i used the current link rather than the forecast link

--- lib/pages/home page.dart
+++ lib/pages/home page.dart
@@ -3,27 +3,200 @@ import 'package:flutter/material.dart';
 import 'search page.dart';
 
 class HomePage extends StatelessWidget {
+  List list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Weather'),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (_) {
-                    return SearchPage();
-                  },
-                ),
-              );
-            },
-            icon: Icon(Icons.search),
+      backgroundColor: Colors.lightBlueAccent,
+      body: SafeArea(
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.lightBlueAccent,
+                Colors.black,
+              ],
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+            ),
           ),
-        ],
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    'City name',
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () =>
+                        MaterialPageRoute(builder: (context) => SearchPage()),
+                    icon: const Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'Today : 4/3',
+                style: TextStyle(
+                  fontSize: 28,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const SizedBox(height: 30),
+              Container(
+                width: 120,
+                height: 120,
+                child: Image.network('*********************************'),
+              ),
+              const SizedBox(height: 30),
+              Text(
+                '44',
+                style: TextStyle(
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Night cloud',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      const Text(
+                        'Min temp',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        '25',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Icon(
+                        Icons.air,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        '25 mph',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Text(
+                        'Max temp',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        '25',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Spacer(),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: Container(
+                  height: 200,
+                  width: MediaQuery.of(context).size.width * 0.95,
+                  decoration: BoxDecoration(
+                    color: Colors.blue.withOpacity(0.3),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: list.map((value) {
+                          return Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 5),
+                            height: 150,
+                            width: 70,
+                            decoration: BoxDecoration(),
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                    bottom: 10,
+                                  ),
+                                  child: Text(
+                                    '2-2',
+                                    style: TextStyle(
+                                        fontSize: 25,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Image.network('src'),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                    top: 10,
+                                  ),
+                                  child: Text(
+                                    '22',
+                                    style: TextStyle(
+                                        fontSize: 25,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        }).toList(),
+                      )),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/services/weather services.dart
+++ lib/services/weather services.dart
@@ -10,12 +10,12 @@ class Weather {
 
   getWeather(String cityName)async{
    // http://api.weatherapi.com/v1/current.json?key=14cec32e400a4edbad4105818230203&q=London&aqi=no
-    Uri url = Uri.parse('$baseUrl/current.json?key=$key&q=$cityName&aqi=no');
+    Uri url = Uri.parse('$baseUrl/forecast.json?key=$key&q=$cityName&aqi=no');
     http.Response response = await http.get(url);
     Map<String,dynamic> data = jsonDecode(response.body);
-    WeatherModel weatherModel = WeatherModel.fromJson(data);
-
-    return weatherModel;
+    ///WeatherModel weatherModel = WeatherModel.fromJson(data);
+    print(data);
+   ///  return weatherModel;
   }
 
 }
